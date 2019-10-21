a8808fb0a11cc7b16ef8a7b709a562fc249df62b
anujjubi <anuj@jubi.ai>
2019-10-21 14:34:50 +0530

    msg

--- static/loader.js
+++ static/loader.js
@@ -128,7 +128,8 @@
         socket: "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js",
         responsiveVoice: "https://code.responsivevoice.org/responsivevoice.js",
         nluComponent: "https://unpkg.com/compromise@latest/builds/compromise.min.js",
-        bundle: "https://parramato.com/bot-view/usaidWeb_353553876735/dev/js/bundle.js",
+        // bundle: "https://parramato.com/bot-view/usaidWeb_353553876735/dev/js/bundle.js",
+        bundle: "https://development.jubi.ai/usaidWeb/js.bundle.js",
         jubiEvents: "https://development.jubi.ai/usaidWeb/js/jubievents.js"
     });
     window.directMultiplier = 1;
