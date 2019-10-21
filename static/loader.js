2413b4082a68be23cecd6c1617d683c78e448483
anujjubi <anuj@jubi.ai>
2019-10-21 14:18:18 +0530

    msg

--- static/loader.js
+++ static/loader.js
@@ -128,7 +128,7 @@
         socket: "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js",
         responsiveVoice: "https://code.responsivevoice.org/responsivevoice.js",
         nluComponent: "https://unpkg.com/compromise@latest/builds/compromise.min.js",
-        bundle: "https://parramato.com/bot-view/usaidWeb_353553876735/dev/js/bundle.js",
+        bundle: "https://parramato.com/bot-view/usaidWeb_353553876735/dev/js/bundle.test.js",
         jubiEvents: "https://development.jubi.ai/usaidWeb/js/jubievents.js"
     });
     window.directMultiplier = 1;
