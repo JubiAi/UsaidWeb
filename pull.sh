b77d2b56913cc37c58dbd658e4b90a76f290d71c
Brenden <brenden@jubi.ai>
2019-10-23 13:29:58 +0530

    msg

--- pull.sh
+++ pull.sh
@@ -1,3 +1,5 @@
-sudo git pull origin master
-sudo pm2 restart usaidWeb
-sudo pm2 logs usaidWeb
+git pull origin master
+pm2 restart web8192 -- 8192
+pm2 restart web8194 -- 8194
+pm2 restart web8196 -- 8196
+pm2 logs
