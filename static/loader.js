5d146aa64e6b4935c28ace108a779d2b15b40aff
kolkatajubi <kolkatachamps@jubi.ai>
2019-11-05 16:21:15 +0530

    msg

--- static/loader.js
+++ static/loader.js
@@ -240,7 +240,7 @@
     // '<img src="https://khushi.jubi.ai/images/close.png" class="img-responsive">'+
     "</div>" +
     '<aside class="jubi-muteUnmuteVoice">' +
-    '<div id="jubi-unmuteVoice" style = "display:block !important">' +
+    '<div id="jubi-unmuteVoice" style = "display:block !important"><p>test</p>' +
     '<img src="https://parramato.com/bot-view/images/unmute.png">' +
     "</div>" +
     '<div id="jubi-muteVoice">' +
