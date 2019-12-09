1218d53d43fe269d25163a06dfcae7165171136b
sayantanh07 <Sayantan61@users.noreply.github.com>
2019-12-09 20:08:22 +0530

    captions

--- adapter/getStarted/post.js
+++ adapter/getStarted/post.js
@@ -4,38 +4,36 @@ var sortBy = require('sort-by')
 var sendExternalMessage = require('../../external.js')
 var request = require('request')
 
-module.exports={
+module.exports = {
 
-	conAge: (data)=>{
-		return new Promise(function(resolve,reject){
+	conAge: (data) => {
+		return new Promise(function (resolve, reject) {
 			console.log(data.data.toLowerCase())
-			if(data.data.toLowerCase() == "i am over 15"){
+			if (data.data.toLowerCase() == "i am over 15") {
 				console.log("I am over 15")
 				data.tags.accepted = true
 				data.tags.rejected = false
 				data.stage = "disclaimer"
-			}
-			else{
+			} else {
 				console.log("I am under 15")
 				data.tags.rejected = true
 				data.tags.accepted = false
 				data.stage = "disclaimer"
 			}
 			console.log("+++++++++++++")
-            console.log(data.tags)
-            console.log("+++++++++++++")
+			console.log(data.tags)
+			console.log("+++++++++++++")
 			return resolve(data)
 		})
 	},
 
-	disclaimer: (data)=>{
-		return new Promise(function(resolve,reject){
+	disclaimer: (data) => {
+		return new Promise(function (resolve, reject) {
 			console.log(data.data)
-			if(data.data.toLowerCase() == "i agree"){
+			if (data.data.toLowerCase() == "i agree") {
 				console.log("I agree")
-				delete data.stage
-			}
-			else{
+				data.stage = "carousalone"
+			} else {
 				console.log("Disagree")
 				data.tags.rejected = true
 				data.stage = "disclaimer"
@@ -44,41 +42,36 @@ module.exports={
 		})
 	},
 
-	ageStage:  data => {
-		return new Promise(function(resolve,reject){
-			if(data.data == "15-24" || parseInt(data.data) >=15 && parseInt(data.data) <=24){
+	ageStage: data => {
+		return new Promise(function (resolve, reject) {
+			if (data.data == "15-24" || parseInt(data.data) >= 15 && parseInt(data.data) <= 24) {
 				console.log("15-24")
 				data.stage = "genderStage"
-			}
-			else if(data.data == "25-34" || parseInt(data.data) >=25 && parseInt(data.data) <=34){
+			} else if (data.data == "25-34" || parseInt(data.data) >= 25 && parseInt(data.data) <= 34) {
 				console.log("25-34")
 				data.stage = "genderStage"
-			}
-			else if(data.data == "35-44" || parseInt(data.data) >=35 && parseInt(data.data) <=44){
+			} else if (data.data == "35-44" || parseInt(data.data) >= 35 && parseInt(data.data) <= 44) {
 				console.log("35-44")
 				data.stage = "genderStage"
-			}
-			else if(data.data == "44+" || parseInt(data.data) >=45 && parseInt(data.data) <=100){
+			} else if (data.data == "44+" || parseInt(data.data) >= 45 && parseInt(data.data) <= 100) {
 				console.log("44+")
 				data.stage = "genderStage"
-			}
-			else if(parseInt(data.data) > 100){
+			} else if (parseInt(data.data) > 100) {
 				data.stage = "ageStage"
 			}
 			resolve(data)
 		})
 	},
 
-	city :(data) => {
+	city: (data) => {
 		console.log(data.data)
-		return new Promise(function(resolve,reject){
-			if(stringSimilarity.findBestMatch(toTitleCase(data.data), cities).bestMatch.rating == 1){
+		return new Promise(function (resolve, reject) {
+			if (stringSimilarity.findBestMatch(toTitleCase(data.data), cities).bestMatch.rating == 1) {
 				data.tags.cityMatches = undefined
 				data.tags.city = toTitleCase(data.data)
 				delete data.stage
 				resolve(data)
-			}
-			else{
+			} else {
 				data.tags.cityMatches = sort(toTitleCase(data.data), cities)
 				resolve(data)
 			}
@@ -86,32 +79,33 @@ module.exports={
 		return data
 	},
 
-} 
-	function toTitleCase(str){
-		 str = str.toLowerCase().split(' ');
-			let final = [ ];
-			for(let word of str){
-			final.push(word.charAt(0).toUpperCase()+ word.slice(1));
-			}
-			return final.join(' ')
-	}
+}
 
-	function sort(inp, data){
-		console.log(data.length)
-		matches = stringSimilarity.findBestMatch(inp, data)
-		arr = []
-		rating = 0.30
-		matches.ratings = matches.ratings.sort(sortBy('-rating'));
-		console.log(matches)
-		while(matches.ratings.length > 9){
-			matches.ratings.forEach(function(match){
-				if(match.rating > rating ){
-					arr.push(match)
-				}
-			})
-			matches.ratings = arr
-			arr = []
-			rating += 0.01
-		}
-		return matches.ratings
+function toTitleCase(str) {
+	str = str.toLowerCase().split(' ');
+	let final = [];
+	for (let word of str) {
+		final.push(word.charAt(0).toUpperCase() + word.slice(1));
 	}
+	return final.join(' ')
+}
+
+function sort(inp, data) {
+	console.log(data.length)
+	matches = stringSimilarity.findBestMatch(inp, data)
+	arr = []
+	rating = 0.30
+	matches.ratings = matches.ratings.sort(sortBy('-rating'));
+	console.log(matches)
+	while (matches.ratings.length > 9) {
+		matches.ratings.forEach(function (match) {
+			if (match.rating > rating) {
+				arr.push(match)
+			}
+		})
+		matches.ratings = arr
+		arr = []
+		rating += 0.01
+	}
+	return matches.ratings
+}
--- adapter/getStarted/pre.js
+++ adapter/getStarted/pre.js
@@ -1,35 +1,79 @@
 var sendExternalMessage = require('../../external.js')
 var request = require('request')
 
-module.exports={
+module.exports = {
 
-	city :(data) =>{
-		return new Promise(function(resolve, reject){
-			if(data.tags.cityMatches && data.tags.cityMatches.length > 0){
+	city: (data) => {
+		return new Promise(function (resolve, reject) {
+			if (data.tags.cityMatches && data.tags.cityMatches.length > 0) {
 				let arr = []
-				data.tags.cityMatches.forEach(function(element){
+				data.tags.cityMatches.forEach(function (element) {
 					arr.push({
-						data : element.target,
-						text : element.target
+						data: element.target,
+						text: element.target
 					})
 				})
-				data.reply={
-					type : "quickReply",
-					text : "Select from one of the below closest match",
-					next : {
-						data : arr
+				data.reply = {
+					type: "quickReply",
+					text: "Select from one of the below closest match",
+					next: {
+						data: arr
 					}
 				}
-			}
-			else if(data.tags.cityMatches && data.tags.cityMatches.length == 0){
-				data.reply={
-					type : 'text',
-					text : 'No city could be found matching '+data.tags.userSays+'. Could you be more specific?'
+			} else if (data.tags.cityMatches && data.tags.cityMatches.length == 0) {
+				data.reply = {
+					type: 'text',
+					text: 'No city could be found matching ' + data.tags.userSays + '. Could you be more specific?'
 				}
 			}
 			resolve(data)
 		})
 	},
+
+
+	carousalOne: (data) => {
+		return new Promise((resolve, reject) => {
+			console.log(data)
+			console.log(data + "-----------------")
+			data.reply = {
+				type: 'generic',
+				text: "Main Menu",
+				next: {
+					data: [{
+							image: flatpills,
+							title: "One of the most preferred birth control methods out there!",
+							buttons: [{
+								type: "text",
+								text: "OCP",
+								data: "ocp"
+							}]
+						},
+						{
+							image: maternity,
+							title: "Planning for the perfect family? Read on!",
+							buttons: [{
+								type: "text",
+								text: "hstp",
+								data: "hstp"
+							}]
+						},
+						{
+							image: iucd,
+							title: "Looking for a long term Birth control option? Check out the superstar IUDs",
+							buttons: [{
+								type: "text",
+								text: "iucd",
+								data: "iucd-d"
+							}]
+						},
+					]
+				}
+			}
+			return resolve(data)
+		})
+	},
+
+
 
 	// city :(data) =>{
 	// 	data.reply={
@@ -48,40 +92,38 @@ module.exports={
 	// 	return data
 	// },
 
-	disclaimer:(data)=>{
-		return new Promise(async function(resolve,reject){
-			if(data.tags.rejected == true && data.tags.accepted == false){
+	disclaimer: (data) => {
+		return new Promise(async function (resolve, reject) {
+			if (data.tags.rejected == true && data.tags.accepted == false) {
 				delete data.tags.rejected
 				await sendExternalMessage(data, 'According to our terms and conditions, you must be over 15 years to access Khushi Live. Read more here https://development.jubi.ai/usaidWeb/termsOfService.html')
 				data.stage = "conAge"
-			}
-			else if(data.tags.accepted == true && data.tags.rejected == false){
+			} else if (data.tags.accepted == true && data.tags.rejected == false) {
 				// delete data.tags.accepted
-				data.reply={
-					type : "button",
-					text : "Great! Btw everything we discuss here is absolutely private. Take a look at your privacy policy and terms of service below to know more. |break|To agree please click on “I agree” below.",
-					next : {
-	                    data : [
-	                    	{
-	                            type: "url",
-                                data: "https://development.jubi.ai/usaidWeb/policyPrivacy.html",
-                                text: "Privacy Policy"
-	                        },
-	                        {
-	                            type: "url",
-                                data: "https://development.jubi.ai/usaidWeb/termsOfService.html",
-                                text: "Terms of Service"
-	                        },
-	                        {
-	                            data : 'I agree',
-	                            text : 'I agree'
-	                        }
-	                    ]
-                	}
+				data.reply = {
+					type: "button",
+					text: "Great! Btw everything we discuss here is absolutely private. Take a look at your privacy policy and terms of service below to know more. |break|To agree please click on “I agree” below.",
+					next: {
+						data: [{
+								type: "url",
+								data: "https://development.jubi.ai/usaidWeb/policyPrivacy.html",
+								text: "Privacy Policy"
+							},
+							{
+								type: "url",
+								data: "https://development.jubi.ai/usaidWeb/termsOfService.html",
+								text: "Terms of Service"
+							},
+							{
+								data: 'I agree',
+								text: 'I agree'
+							}
+						]
+					}
 				}
 				delete data.stage
 			}
-		resolve(data)
+			resolve(data)
 		})
 	}
 }
