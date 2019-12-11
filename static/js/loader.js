e233bec73e516a4802cb61b5da14ff9c9c77c52f
RodricHil <rodrickhilarian77@gmail.com>
2019-12-12 00:52:50 +0530

    mute unmute

--- static/js/loader.js
+++ static/js/loader.js
@@ -355,7 +355,28 @@
     // '<div class="stop-recording">Listening...</div>' +
     // "</div>" +
     // "</div>" +
+    '<aside class="jubi-muteUnmuteVoice">' +
+    '<div id="jubi-unmuteVoice">' +
+    '<img src="https://parramato.com/bot-view/images/unmute.png">' +
+    "</div>" +
+    '<div id="jubi-muteVoice">' +
+    '<img src="https://parramato.com/bot-view/images/mute.png">' +
+    "</div>" +
+    "</aside>" +
     '<textarea id="jubi-answerBottom" placeholder="Type a question here..." style="resize:none;overflow:hidden;" autofocus></textarea> ' +
+    '<div class="voice-buttons" id="voice-buttons">' +
+    '<div class="voiceIcon" id="button-play-ws">' +
+    '<img src="./images/voice.png" class="img-fluid">' +
+    "</div>" +
+    '<div class="voicePulse" id="button-stop-ws">' +
+    '<div class="sk-three-bounce">' +
+    '<div class="sk-child sk-bounce1"></div>' +
+    '<div class="sk-child sk-bounce2"></div>' +
+    '<div class="sk-child sk-bounce3"></div>' +
+    "</div>" +
+    '<div class="stop-recording">Listening...</div>' +
+    "</div>" +
+    "</div>" +
     "</div>" +
     '<div class="datasendButtons">' +
     '<div class="sendIcon" id="button-send">' +
