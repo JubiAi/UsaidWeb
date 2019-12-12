b35d4e15e44546d64e347077ca9be64d21da6407
RodricHil <rodrickhilarian77@gmail.com>
2019-12-12 13:10:51 +0530

    show hide voice icon

--- static/js/jubievents.js
+++ static/js/jubievents.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     $(".dropdown-header").toggle();
   });
   $("#jubi-answerBottom").click(function() {
-    $(".voiceIcon button-play-ws-right").hide();
+    $(".voiceIcon").hide();
     $(".jubi-sec_newFooter button").show();
   });
 });
