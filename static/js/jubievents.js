895548fed48ac33443c23cdf677ba96fb1773a5b
RodricHil <rodrickhilarian77@gmail.com>
2019-12-12 13:22:37 +0530

    focus

--- static/js/jubievents.js
+++ static/js/jubievents.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     $(".dropdown-header").toggle();
   });
   $("#jubi-answerBottom").focus(function() {
-    $(".voiceIcon").css("display", "none");
-    $(".jubi-sec_newFooter button").css("display", "inline");
+    $(".voiceIcon").css("display", "inline");
+    // $(".jubi-sec_newFooter button").show();
   });
 });
