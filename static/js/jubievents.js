6a4c52f482862aaa3516f8911663ed5498d4e79a
RodricHil <rodrickhilarian77@gmail.com>
2019-12-17 12:56:05 +0530

    ipad

--- static/js/jubievents.js
+++ static/js/jubievents.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
     $("#voice-buttons").show();
   });
 
-  let testExp = new RegExp("iPhone" + "iPad", "i");
+  let testExp = new RegExp("iPhone|iPad|", "i");
   if (testExp.test(navigator.userAgent)) {
     console.log("test");
     $("#button-play-ws").hide();
