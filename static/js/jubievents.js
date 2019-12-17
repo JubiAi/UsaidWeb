c06258e508b5dd12ee2fad5c754db41e67c8c8d2
RodricHil <rodrickhilarian77@gmail.com>
2019-12-17 15:34:48 +0530

    hello

--- static/js/jubievents.js
+++ static/js/jubievents.js
@@ -61,6 +61,12 @@ $(document).ready(function() {
   } else {
     console.log("msg");
   }
-});
 
-// button - send;
+  textarea = document.querySelector("#jubi-answerBottom");
+  textarea.addEventListener("input", autoResize, false);
+
+  function autoResize() {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+  }
+});
