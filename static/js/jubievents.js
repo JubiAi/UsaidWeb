0bb4a946c6a0deee02404288b2c7c97615ac2486
RodricHil <rodrickhilarian77@gmail.com>
2019-12-17 13:09:13 +0530

    msg

--- static/js/jubievents.js
+++ static/js/jubievents.js
@@ -43,6 +43,10 @@ $(document).ready(function() {
       $("#button-play-ws").hide();
       $("#voice-buttons").hide();
     });
+    $("body").on("focusout", "#jubi-answerBottom", function() {
+      $("#button-play-ws").hide();
+      $("#voice-buttons").hide();
+    });
   } else {
     console.log("msg");
   }
