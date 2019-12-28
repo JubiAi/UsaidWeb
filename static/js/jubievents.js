d39f7212bac707f9cc0ab2b86ee6cde807d2457d
RodricHil <rodrickhilarian77@gmail.com>
2019-12-28 14:45:09 +0530

    send button

--- static/js/jubievents.js
+++ static/js/jubievents.js
@@ -29,6 +29,7 @@ $(document).ready(function() {
     $("#jubi-bottomClick").attr("style", "display: block !important");
   });
   $("body").on("focusout", "#jubi-answerBottom", function() {
+    $("#jubi-bottomClick").hide();
     $(".voiceIcon").show();
     $("#voice-buttons").show();
   });
