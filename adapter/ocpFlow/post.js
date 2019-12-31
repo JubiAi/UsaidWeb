bf9928a8789aaabb47021d038d94a874b8c0e592
BrendenJubi <brenden@jubi.ai>
2019-12-31 10:42:03 +0530

    new

--- adapter/ocpFlow/post.js
+++ adapter/ocpFlow/post.js
@@ -1,11 +1,11 @@
 module.exports = {
 	q1: (model) => {
 		if (model.data.toLowerCase().includes("myth")) {
-			model.tags.answer1 = false
+			model.tags.answer1 = true
 			delete model.stage
 		} 
 		else if (model.data.toLowerCase().includes("fact")) {
-			model.tags.answer1 = true
+			model.tags.answer1 = false
 			delete model.stage
 		}
 		return model
@@ -13,11 +13,11 @@ module.exports = {
 
 	q2: (model) => {
 		if (model.data.toLowerCase().includes("myth")) {
-			model.tags.answer2 = false
+			model.tags.answer2 = true
 			delete model.stage
 		} 
 		else if (model.data.toLowerCase().includes("true")) {
-			model.tags.answer2 = true
+			model.tags.answer2 = false
 			delete model.stage
 		}
 		return model
@@ -25,11 +25,11 @@ module.exports = {
 
 	q3: (model) => {
 		if (model.data.toLowerCase().includes("not")) {
-			model.tags.answer3 = false
+			model.tags.answer3 = true
 			delete model.stage
 		} 
 		else if (model.data.toLowerCase().includes("true")) {
-			model.tags.answer3 = true
+			model.tags.answer3 = false
 			delete model.stage
 		}
 		return model
--- adapter/ocpFlow/pre.js
+++ adapter/ocpFlow/pre.js
@@ -1,6 +1,6 @@
 module.exports={
     q2: model => {
-        if (model.tags.answer1 == false) {
+        if (model.tags.answer1 == true) {
             model.reply = {
                 type: "quickReply",
                 text: "Correct! Oral contraceptive pills (OCPs) are temporary forms of birth control. Once a woman stops taking the pill, within 1 to 3 months her fertility returns.|break|Next one, if you get pregnant when you are using these pills, your babies can have birth defects.",
@@ -18,7 +18,7 @@ module.exports={
                 }
             };
         } 
-        else if (model.tags.answer1 == true) {
+        else if (model.tags.answer1 == false) {
             model.reply = {
                 type: "quickReply",
                 text: "This is actually a myth, my friend! You see, Oral contraceptive pills (OCPs) are only temporary forms of birth control. Within 1 to 3 months of stopping the pills, a woman’s fertility returns.|break| Next one, if you get pregnant when you are using these pills, your babies can have birth defects.",
@@ -41,7 +41,7 @@ module.exports={
     },
     
     q3: model => {
-        if (model.tags.answer2 == false) {
+        if (model.tags.answer2 == true) {
             model.reply = {
                 type: "quickReply",
                 text: "True! Even if a woman accidentally takes an OCP when she is pregnant, the baby will not be born with birth defects. OCPs do not harm the fetus or the baby. |break|Lastly, Any kind of OCP is not safe for breastfeeding mothers.",
@@ -60,7 +60,7 @@ module.exports={
                 }
             };
         } 
-        else if (model.tags.answer2 == true) {
+        else if (model.tags.answer2 == false) {
             model.reply = {
                 type: "quickReply",
                 text: "Oh! That’s actually a myth. Even if a woman accidentally takes an OCP when she is pregnant, the baby will not be bornwith birth defects. OCPs do not harm the fetus or the baby. |break|Lastly, Any kind of OCP is not safe for breastfeeding mothers.",
@@ -83,7 +83,7 @@ module.exports={
     },
     
     q4: model => {
-        if (model.tags.answer3 == false) {
+        if (model.tags.answer3 == true) {
             model.reply = {
                 type: "quickReply",
                 text: "Correct! So, Non-hormonal and Progesterone (a certain type of hormone) only pills can be safely used by breastfeeding mothers! They don’t affect the breast milk adversely.",
@@ -102,7 +102,7 @@ module.exports={
                 }
             };
         }
-        else if (model.tags.answer3 == true) {
+        else if (model.tags.answer3 == false) {
             model.reply = {
                 type: "quickReply",
                 text: "Uh-oh, Wrong answer! Non-hormonal and Progesterone (a certain type of hormone) only pills can be safely used by breastfeeding mothers! They don’t affect the breast milk adversely. Done! I’m sure you’ve learnt something new.To learn more click on the button below. You can also type in your query in the text box below!",
