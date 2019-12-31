19a4f087b22e81385ed541d6c689316a5086c6bc
BrendenJubi <brenden@jubi.ai>
2019-12-31 12:58:18 +0530

    new

--- adapter/iucd/pre.js
+++ adapter/iucd/pre.js
@@ -2,7 +2,7 @@ module.exports={
 	final : model=>{
 		model.reply={
 			type : "quickReply",
-			text : "Wow! You’ve learned a lot today. For more help with contraceptives, you can get in touch with our counselors. Got a query? you can type it in the text box below. Or you can continue with another topic by clicking on main menu!"
+			text : "Wow! You’ve learned a lot today. For more help with contraceptives, you can get in touch with our counselors. Got a query? you can type it in the text box below. Or you can continue with another topic by clicking on main menu!",
 			next : {
 				data : [
 					{
