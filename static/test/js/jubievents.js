8f3d5c9c1ab1b67e2d04d12e22b9be25523ff3d7
RodricHil <rodrickhilarian77@gmail.com>
2020-01-07 19:16:05 +0530

    test

--- static/test/js/jubievents.js
+++ static/test/js/jubievents.js
@@ -110,10 +110,10 @@ $(document).ready(function() {
         n < 26 ? "26px" : n + "px"),
       "" == t &&
         (document.getElementById("jubi-answerBottom").style.height = "26px"),
-      // (document.getElementById("jubi-bxinput").style.height =
-      //   n < 53 ? "53px" : n + "px"),
-      // "" == t &&
-      //   (document.getElementById("jubi-bxinput").style.height = "53px"),
+      (document.getElementById("jubi-bxinput").style.height =
+        n < 53 ? "53px" : n + "px"),
+      "" == t &&
+        (document.getElementById("jubi-bxinput").style.height = "53px"),
       // (document.getElementById("jubi-textInput").style.height =
       //   n < 93 ? "93px" : n + "px"),
       // "" == t &&
